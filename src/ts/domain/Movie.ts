ce30b681f34886d0e00a182731531d8748a6c875
Dmitry_home <79811924115@yandex.ru>
2022-10-22 10:55:19 +0300

    ts task1 v.1

---
+++ src/ts/domain/Movie.ts
@@ -0,0 +1,15 @@
+import Buyable from "./Buyable";
+
+export default class Movie implements Buyable {
+  constructor(
+    readonly img: string,
+    readonly name: string,
+    readonly quality: string,
+    readonly EngName: string,
+    readonly year: number,
+    readonly country: string,
+    readonly slogan: string,
+    readonly genres: [string, string, string, string, string],
+    readonly time: string,
+  ){}
+}
